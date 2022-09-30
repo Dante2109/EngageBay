ae3a2f20f47f244d52eeaab2bf2784a5de1b1abf
Dante2109 <akshayverma4299@gmail.com>
2022-09-30 14:15:51 +0530

    day3 and somework of day-4

--- navbar.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener("scroll", function(){
-    console.log("hello")
-    let header=document.querySelector("#navbar");
-    header.classList.toggle("sticky", window.scrollY > 0)
-})
